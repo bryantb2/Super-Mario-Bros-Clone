ad1cb5e5919d8ab254009b61591c8e2b86a9c593
Blake <bryantb2@my.lanecc.edu>
2020-08-22 14:58:03 -0700

    built out initial boilerpalte

---
+++ src/boilerplate/storeConfig.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+
+// builder logger enhancer
+const logger = store => next => action => {
+    console.group(action.type)
+    console.info('dispatching', action)
+    let result = next(action)
+    console.log('next state', store.getState())
+    console.groupEnd()
+    return result
+};
+
+// apply middleWares
+const middleWares = [reduxThunk];
+if (process.env.REACT_APP_NODE_ENV === 'development') middleWares.push(logger);
+// create store
+const store = createStore(rootReducer, applyMiddleware(...middleWares));
+
+export default (props) => (
+    <Provider store={store}>
+        {props.children}
+    </Provider>
+);
